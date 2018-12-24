3acb3b644ef4631d68e430abfade3d2d513c3877
jameseclement <jameseclement@gmail.com>
2018-12-24 17:44:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,14 +1,14 @@
 def create_an_empty_array
-  
+  empty_array = [];
   
 end
 
 def create_an_array
-  
+  array = [];
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push[element];
 end
 
 def add_element_to_start_of_array(array, element)
